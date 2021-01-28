03e949afbc58406852182e80f93facf3db23f663
ThiagoFiorelli <36682785+ThiagoFiorelli@users.noreply.github.com>
2021-01-28 14:17:54 -0300

    Created category model and category model tests

---
+++ models/category.py
@@ -0,0 +1,39 @@
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import validates
+import re
+
+
+Base = declarative_base()
+
+class Category(Base):
+    __tablename__ = 'categories'
+    id_ = Column('id', Integer, primary_key = True)
+    name = Column('name', String(length=100), nullable = False)
+    description = Column('description', String(length=255), nullable = False)
+
+    def __init__(self, name: str, description: str) -> None:
+        self.name = name
+        self.description = description
+
+    @validates('name')
+    def validate_name(self, key, name):
+        print(name)
+        if not isinstance(name, str):
+            raise TypeError('Name must be a string!')
+        if not name:
+            raise ValueError('Name cannot be empty!')
+        if len(name) > 100:
+            raise ValueError('Name must be 100 or less characters!')
+        if not re.match(r'^[a-zà-úA-ZÀ-Ú0-9 ]+$', name):
+            raise ValueError('Name cannot have special characters!')
+        return name
+
+    @validates('description')
+    def validate_description(self, key, description):
+        if not isinstance(description, str):
+            raise TypeError('Description must be a string!')
+        if len(description) > 255:
+            raise ValueError('Description must be 255 or less characters!')
+        return description
+
